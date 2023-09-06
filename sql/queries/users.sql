bf32fa5da5e983c9c3f33070ff3de07ea0cc514e
oggy107 <urmalsingh10@gmail.com>
2023-09-06 19:22:37 +0530

    modified query to add auto generated api_key

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,5 +1,5 @@
 -- name: CreateUser :one
-INSERT INTO users(id, created_at, updated_at, name) VALUES(uuid_generate_v4(), NOW(), NOW(), $1)
+INSERT INTO users(id, created_at, updated_at, name, api_key) VALUES(uuid_generate_v4(), NOW(), NOW(), $1, encode(sha256(random()::text::bytea), 'hex'))
 RETURNING *;
 
 -- name: GetUsers :many
