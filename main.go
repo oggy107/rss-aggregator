99efd95084945f5665885ae766f7d20f4c8c3eb4
oggy107 <urmalsingh10@gmail.com>
2023-09-06 19:21:56 +0530

    added database connection and add user route

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/oggy107/rss-aggregator/internal/database"
 )
 
 func runServer(router *chi.Mux) {
@@ -16,7 +20,7 @@ func runServer(router *chi.Mux) {
 	SERVER_ADDR := SERVER_NAME + ":" + PORT
 
 	if PORT == "" {
-		log.Fatalln("PORT is not set")
+		logFatal("PORT is not set")
 	}
 
 	log.Println("Starting server at:", SERVER_ADDR)
@@ -26,23 +30,63 @@ func runServer(router *chi.Mux) {
 	}
 }
 
+func connectDB() (conn *sql.DB) {
+	CONNECTION_STRING := os.Getenv("POSTGRES_CONNECTION")
+
+	if CONNECTION_STRING == "" {
+		logFatal("Database connection string is not set")
+	}
+
+	conn, err := sql.Open("postgres", CONNECTION_STRING)
+
+	if err != nil {
+		logFatal(err.Error())
+	}
+
+	return conn
+}
+
+type ApiConfig struct {
+	DB *database.Queries
+}
+
+// middleware to pass database using context to all handlers
+func (cfg *ApiConfig) ApiMiddleware(next http.Handler) http.Handler {
+	fun := func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "config", cfg)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+
+	return http.HandlerFunc(fun)
+}
+
 func main() {
 	godotenv.Load(".env")
 
+	conn := connectDB()
+	defer conn.Close()
+
+	apiConfig := &ApiConfig{
+		DB: database.New(conn),
+	}
+
 	router := chi.NewRouter()
+
+	router.Use(apiConfig.ApiMiddleware)
 
 	handler := &Handler{
 		v1: &V1{},
 	}
 
 	router.Get("/ping", handler.pong)
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/v1", http.StatusFound)
-	})
+	// router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.Redirect(w, r, "/v1", http.StatusFound)
+	// })
 
 	router.Route("/v1", func(v1 chi.Router) {
-		v1.Get("/", handler.v1.root)
-		v1.Get("/users", handler.v1.users)
+		// v1.Get("/", handler.v1.root)
+		v1.Post("/user", handler.v1.createUser)
 	})
 
 	runServer(router)
